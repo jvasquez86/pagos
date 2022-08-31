44c7251b168d44c751f90b76d02775e74221020e
Jorge Vásquez <jorge.vasquez.echeverria@gmail.com>
2022-08-31 18:24:19 -0400

    Creacion de componentes y nav

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -1,12 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-    Blog
-</body>
-</html>
+<x-layouts.app title="Blog" meta-description="Blog de pagina">
+    <h1>Blog</h1>
+</x-layouts.app>
---
+++ resources/views/components/layouts/app.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>{{ $title }}</title>
+    <meta name="" content="{{ $metaDescription }}">
+
+</head>
+
+<body>
+    <x-layouts.nav />
+    {{ $slot }}
+</body>
+
+</html>
